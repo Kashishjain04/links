fce8d1716ca8d0bd607d9824489c17702e83308e
KashishJain <55389276+Kashishjain04@users.noreply.github.com>
2022-01-30 22:54:59 +0530

    custom slug feature

--- pages/api/shorten.js
+++ pages/api/shorten.js
@@ -5,17 +5,35 @@ import requireLogin from "./middlewares/requireLogin";
 import { ObjectId } from "mongodb";
 
 const handler = async (req, res) => {
+	// check if request is authenticated
 	await runMiddleware(req, res, requireLogin);
 	if (!req.user) return res.status(401).json({ error: "Not logged in" });
-	const { longUrl } = JSON.parse(req.body);
-	if (!longUrl) {
-		res.status(400).json({ error: "Please provide a valid URL" });
-	}
-	const shortUrl = shortid.generate().toLowerCase();
 
+	// request body validation
+	const { longUrl, customSlug } = JSON.parse(req.body);
+	if (!longUrl) {
+		return res.status(400).json({ error: "Please provide a valid URL" });
+	}
+
+	//initialize db
 	const client = await clientPromise,
 		db = client.db(process.env.MONGODB_DB);
 
+	// custom slug validation
+	if(customSlug){
+		if(customSlug.length > 9){
+			return res.status(400).json({ error: "Custom slug must be 9 characters or less" });
+		}
+		const slugExists = await db.collection("links").findOne({ short: customSlug });
+		if(slugExists){
+			return res.status(400).json({ error: "Custom slug already exists" });
+		}
+	}
+	
+	// generate short url
+	const shortUrl = customSlug || shortid.generate().toLowerCase();
+
+	// check if url already exists
 	const doc = await db.collection("links").findOneAndUpdate(
 		{ long: longUrl },
 		{
@@ -26,6 +44,7 @@ const handler = async (req, res) => {
 	if (doc.value) {
 		return res.status(200).json({ short: doc.value.short, long: doc.value.long });
 	} else {
+		// insert new url
 		const doc = await db.collection("links").insertOne({
 			long: longUrl,
 			short: shortUrl,
